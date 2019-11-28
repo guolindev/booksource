5960505c5a5ceb88317fefed905948c80dc009ad
scott <qvis.be@gmail.com>
2019-11-28 13:23:00 +0800

    1.补充代码 2.去除警告

--- chapter2/ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.java
+++ chapter2/ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.activitylifecycletest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+//       如果回收情况下，取得回收activity前保存的数据。
+        if (savedInstanceState != null) {
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d(TAG, tempData);
+        }
+
         Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
         Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
         startNormalActivity.setOnClickListener(new View.OnClickListener() {
@@ -70,4 +77,16 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onRestart");
     }
 
+    /**
+     * 如果Activity在后台被回收，需要在这里保存状态值。此方法活动回收前一定会被调用。
+     * 比如：输入参数值等。
+     * @param outState
+     *
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "Something you just typed";
+        outState.putString("data_key", tempData);
+    }
 }
