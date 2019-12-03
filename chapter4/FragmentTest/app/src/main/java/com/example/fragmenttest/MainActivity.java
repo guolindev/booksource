ace18d3e834d8835bf0b428e8dc95503c7272e4e
scott <qvis.be@gmail.com>
2019-12-03 15:24:26 +0800

    适配本地gradle，SDK版本。

--- chapter4/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ chapter4/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.fragmenttest;
 
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +19,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
-//        replaceFragment(new RightFragment());
+        replaceFragment(new RightFragment());
+        Configuration config = getResources().getConfiguration();
+        int  smallestScreenWidth = config.smallestScreenWidthDp;
+        Log.d(TAG, "smallestScreenWidthDp:"+smallestScreenWidth);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-//                replaceFragment(new AnotherRightFragment());
+                replaceFragment(new AnotherRightFragment());
                 break;
             default:
                 break;
         }
     }
 
-//    private void replaceFragment(Fragment fragment) {
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.right_layout, fragment);
-//        transaction.addToBackStack(null);
-//        transaction.commit();
-//    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
 }
