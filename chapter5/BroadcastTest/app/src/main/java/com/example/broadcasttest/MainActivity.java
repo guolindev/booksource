305f79013a0db9307f52f70a4e0d7fd18225cb89
scott <qvis.be@gmail.com>
2019-12-04 13:57:12 +0800

    sdk大于26的版本增强安全性，会拦截隐式广播。故示例选择sdk24版本。

--- chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -21,17 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
     private LocalBroadcastManager localBroadcastManager;
 
-    private NetworkChangeReceiver networkChangeReceiver;
+//    private NetworkChangeReceiver networkChangeReceiver;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
 
         // 获取实例
         localBroadcastManager = LocalBroadcastManager.getInstance(this);
@@ -39,16 +39,30 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent("com.example.broadcasttest.LOCAL_BROADCAST");
                 // 发送本地广播
                 localBroadcastManager.sendBroadcast(intent);
+
+//                //用于测试外部app接收广播
+//                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+//                //发送全局广播
+//                //sendBroadcast(intent);
+//                //发送顺序广播
+//                sendOrderedBroadcast(intent,null);
+
             }
         });
+
+          // 在代码里动态注册。
         intentFilter = new IntentFilter();
         intentFilter.addAction("com.example.broadcasttest.LOCAL_BROADCAST");
         localReceiver = new LocalReceiver();
         // 注册本地广播监听器
         localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+
+        //用于测试外部app接收广播的示例
+//        intentFilter.addAction("com.example.broadcasttest.MY_BROADCAST");
     }
 
     @Override
@@ -67,23 +81,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class NetworkChangeReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectionManager = (ConnectivityManager)
-                    getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isAvailable()) {
-                Toast.makeText(context, "network is available",
-                        Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(context, "network is unavailable",
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        }
-
-    }
+//    class NetworkChangeReceiver extends BroadcastReceiver {
+//
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            ConnectivityManager connectionManager = (ConnectivityManager)
+//                    getSystemService(Context.CONNECTIVITY_SERVICE);
+//            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+//            if (networkInfo != null && networkInfo.isAvailable()) {
+//                Toast.makeText(context, "network is available",
+//                        Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(context, "network is unavailable",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }
+//
+//    }
 
 }
--- chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
+++ chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -10,7 +10,9 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
-        abortBroadcast();
+
+        //顺序广播情况下可以终止广播
+//        abortBroadcast();
     }
 
 }
