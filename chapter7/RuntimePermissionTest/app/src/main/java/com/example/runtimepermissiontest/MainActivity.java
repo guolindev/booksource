4d9839b6daf616254da57b53607def52db7cb2ad
scott <qvis.be@gmail.com>
2019-12-09 23:18:06 +0800

    1.适配本地sdk和gradle。 2.补充例子代码。

--- chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -4,11 +4,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,12 +22,24 @@ public class MainActivity extends AppCompatActivity {
         makeCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_CALL);
-                    intent.setData(Uri.parse("tel:10086"));
-                    startActivity(intent);
-                } catch (SecurityException e) {
-                    e.printStackTrace();
+
+//方式1：直接拨打
+//                try {
+//                    Intent intent = new Intent(Intent.ACTION_CALL);
+//                    intent.setData(Uri.parse("tel:10086"));
+//                    startActivity(intent);
+//                } catch (SecurityException e) {
+//                    e.printStackTrace();
+//                }
+
+//方式2：弹出选择对话框
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                        permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new
+                            String[]{Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    call();
+
                 }
             }
         });
