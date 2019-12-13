98c61e9d01039b89b0cd639a48b91b0d9a6760f9
scott <qvis.be@gmail.com>
2019-12-13 12:21:00 +0800

    测试用例也切换到androidx下

--- chapter6/SharedPreferencesTest/app/src/androidTest/java/com/example/sharedpreferencestest/ApplicationTest.java
+++ chapter6/SharedPreferencesTest/app/src/androidTest/java/com/example/sharedpreferencestest/ApplicationTest.java
@@ -1,13 +1,28 @@
 package com.example.sharedpreferencestest;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = ApplicationProvider.getApplicationContext();
+        assertEquals("com.example.sharedpreferencestest", appContext.getPackageName());
     }
+
 }
